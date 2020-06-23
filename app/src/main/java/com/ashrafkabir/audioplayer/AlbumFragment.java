e0d486092fecb8f2bd5adc0e5de6cd50fff6af15
Ashraf Kabir <ashrafkabir95@gmail.com>
2020-06-23 20:04:41 +0600

    album fragment implementing

--- app/src/main/java/com/ashrafkabir/audioplayer/AlbumFragment.java
+++ app/src/main/java/com/ashrafkabir/audioplayer/AlbumFragment.java
@@ -3,10 +3,15 @@ package com.ashrafkabir.audioplayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import static com.ashrafkabir.audioplayer.MainActivity.musicFiles;
 
 
 /**
@@ -14,6 +19,8 @@ import android.view.ViewGroup;
  */
 public class AlbumFragment extends Fragment {
 
+    RecyclerView recyclerView;
+    AlbumAdapter albumAdapter;
     public AlbumFragment() {
         // Required empty public constructor
     }
@@ -23,7 +30,14 @@ public class AlbumFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_album, container, false);
+        View view = inflater.inflate(R.layout.fragment_album, container, false);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        if (!(musicFiles.size() < 1)) {
+            albumAdapter = new AlbumAdapter(getContext(), musicFiles);
+            recyclerView.setAdapter(albumAdapter);
+            recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        }
         return view;
     }
 }
